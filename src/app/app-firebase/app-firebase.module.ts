2e5283b1423007bf1ebd45f65ed19ca7b9b008c7
Rajat Badjatya <rajat.badjatya@tcs.com>
2018-07-07 23:14:50 +0530

    code restructuring

--- src/app/app-firebase/app-firebase.module.ts
+++ src/app/app-firebase/app-firebase.module.ts
@@ -1,18 +1,16 @@
 import { environment } from './../../environments/environment.prod';
 import { NgModule } from '@angular/core';
-
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuth } from 'angularfire2/auth';
-
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuth
+    AngularFireAuthModule
   ],
   exports: [
     AngularFireModule,
-    AngularFireAuth
+    AngularFireAuthModule,
   ]
 })
 export class AppFirebaseModule { }
